6c63d0390f24e5b046586cf3ed655c5a4531dd12
William Carvalho <thelordl2@hotmail.com>
2021-07-13 19:40:32 -0300

    Criando operação de instanciação

--- src/main/java/com/acwilliam/projetomc/ProjetomcApplication.java
+++ src/main/java/com/acwilliam/projetomc/ProjetomcApplication.java
@@ -1,13 +1,34 @@
 package com.acwilliam.projetomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.acwilliam.projetomc.domain.Categoria;
+import com.acwilliam.projetomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ProjetomcApplication {
+public class ProjetomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	CategoriaRepository categoriaRespository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRespository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
+	
+	
 }
