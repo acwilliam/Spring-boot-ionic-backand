acdada67b65bdc1e202b293d882b63871c7840f1
William Carvalho <thelordl2@hotmail.com>
2021-07-22 19:19:06 -0300

    ItemPedido e ItemPedidoPK - versao 2

--- src/main/java/com/acwilliam/projetomc/ProjetomcApplication.java
+++ src/main/java/com/acwilliam/projetomc/ProjetomcApplication.java
@@ -13,6 +13,7 @@ import com.acwilliam.projetomc.domain.Cidade;
 import com.acwilliam.projetomc.domain.Cliente;
 import com.acwilliam.projetomc.domain.Endereco;
 import com.acwilliam.projetomc.domain.Estado;
+import com.acwilliam.projetomc.domain.ItemPedido;
 import com.acwilliam.projetomc.domain.Pagamento;
 import com.acwilliam.projetomc.domain.PagamentoComBoleto;
 import com.acwilliam.projetomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.acwilliam.projetomc.repositories.CidadeRepository;
 import com.acwilliam.projetomc.repositories.ClienteRepository;
 import com.acwilliam.projetomc.repositories.EnderecoRepository;
 import com.acwilliam.projetomc.repositories.EstadoRepository;
+import com.acwilliam.projetomc.repositories.ItemPedidoRepository;
 import com.acwilliam.projetomc.repositories.PagamentoRepository;
 import com.acwilliam.projetomc.repositories.PedidoRepository;
 import com.acwilliam.projetomc.repositories.ProdutoRepository;
@@ -48,6 +50,8 @@ public class ProjetomcApplication implements CommandLineRunner {
 	PedidoRepository pedidoRepository;
 	@Autowired
 	PagamentoRepository pagamentoRepository;
+	@Autowired
+	ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetomcApplication.class, args);
@@ -114,6 +118,19 @@ public class ProjetomcApplication implements CommandLineRunner {
 				
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.0, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip3));
+		ped2.getItens().addAll(Arrays.asList(ip2));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 		
 	}
 	
--- src/main/java/com/acwilliam/projetomc/domain/Produto.java
+++ src/main/java/com/acwilliam/projetomc/domain/Produto.java
@@ -2,8 +2,10 @@ package com.acwilliam.projetomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,8 +14,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Produto implements Serializable {
@@ -33,6 +37,9 @@ public class Produto implements Serializable {
 			)
 	public List<Categoria> categorias = new ArrayList<>();//iniciando a coleção
 	
+	@OneToMany(mappedBy="id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
+	
 	public Produto() {
 	}
 
@@ -41,6 +48,15 @@ public class Produto implements Serializable {
 		this.id = id;
 		this.name = name;
 		this.price = price;
+	}
+	
+	@OneToMany(mappedBy="id.produto")
+	public List<Pedido> getPedidos(){
+		List<Pedido> lista = new ArrayList<>();
+		for (ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
 	}
 
 	public Integer getId() {
@@ -74,7 +90,15 @@ public class Produto implements Serializable {
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
 	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
 
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -91,6 +115,5 @@ public class Produto implements Serializable {
 		Produto other = (Produto) obj;
 		return Objects.equals(id, other.id);
 	}
-	
 	
 }
