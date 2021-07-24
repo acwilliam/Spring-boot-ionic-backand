678d3cf1dcb549408604c74fd30a4be3cab3d4ab
William Carvalho <thelordl2@hotmail.com>
2021-07-23 23:37:32 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/acwilliam/projetomc/domain/Cliente.java
+++ src/main/java/com/acwilliam/projetomc/domain/Cliente.java
@@ -50,7 +50,7 @@ public class Cliente implements Serializable{
 		this.name = name;
 		this.email = email;
 		CpfOuCnpj = cpfOuCnpj;
-		this.tipo = tipo.getCod();
+		this.tipo = (tipo==null) ? null : tipo.getCod();
 	}
 
 	public Integer getId() {
