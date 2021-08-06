2246ee6841508d97ae01f9e70579a560ba1eb536
William Carvalho <thelordl2@hotmail.com>
2021-08-06 20:36:49 -0300

    Salvando perfis de usuário na base de dados

--- src/main/java/com/acwilliam/projetomc/services/DBService.java
+++ src/main/java/com/acwilliam/projetomc/services/DBService.java
@@ -20,6 +20,7 @@ import com.acwilliam.projetomc.domain.PagamentoComCartao;
 import com.acwilliam.projetomc.domain.Pedido;
 import com.acwilliam.projetomc.domain.Produto;
 import com.acwilliam.projetomc.domain.enums.EstadoPagamento;
+import com.acwilliam.projetomc.domain.enums.Perfil;
 import com.acwilliam.projetomc.domain.enums.TipoCliente;
 import com.acwilliam.projetomc.repositories.CategoriaRepository;
 import com.acwilliam.projetomc.repositories.CidadeRepository;
@@ -119,15 +120,22 @@ public class DBService {
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 		Cliente cli1 = new Cliente(null, "Cris Baptistela", "cristianqx@gmail.com", "36378912377",TipoCliente.PESSOAFISICA, pe.encode("123") );
+		cli1.addPerfil(Perfil.CLIENTE);
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Cliente cli2 = new Cliente(null, "will coelho", "acwilliammsn@gmail.com", "30624282015",TipoCliente.PESSOAFISICA, pe.encode("123") );
+		cli2.getTelefones().addAll(Arrays.asList("959894658","456585465"));
+		cli2.addPerfil(Perfil.ADMIN);
 		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "apto 203", "Jardim", "37851605857", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777014562", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida river", "405", "ap 800", "Centro", "7894568456", cli2, c2);
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 		
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy HH:mm");
 		
